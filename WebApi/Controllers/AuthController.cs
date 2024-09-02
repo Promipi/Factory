65bbd9d179ef91b4dae0890437ab52658ebb7402
Cesar Alejandro Reina Morinigo <cesar@rugertek.com>
2024-09-02 01:05:37 -0400

    Login and SignUp Implemented

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -1,9 +1,14 @@
-﻿using Common.Core.Domain;
+﻿using Common.Core.Contracts.Auth;
+using Common.Core.Domain;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebApi.Configuration.Settings;
 
 namespace WebApi.Controllers
@@ -13,24 +18,95 @@ namespace WebApi.Controllers
     public sealed class AuthController : ControllerBase
     {
         private readonly DataContext _context;
-        //private readonly SignInManager<Customer> _signInManager;
+        private readonly SignInManager<Customer> _signInManager;
         private readonly IOptions<JwtSettings> _jwtSettings;
         private readonly IConfiguration _configuration;
 
-        public AuthController(DataContext context, /*SignInManager<Customer> signInManager*/ IOptions<JwtSettings> jwtSettings, IConfiguration configuration)
+        public AuthController(DataContext context, SignInManager<Customer> signInManager,IOptions<JwtSettings> jwtSettings, IConfiguration configuration)
         {
             _context = context;
-            //_signInManager = signInManager;
+            _signInManager = signInManager;
             _jwtSettings = jwtSettings;
             _configuration = configuration;
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginRequest request,
+        [HttpPost("LogIn")]
+        public async Task<IActionResult> Login([FromBody] LoginDto request,
             CancellationToken cancellationToken = default)
         {
-            //var user = await _signInManager.UserManager.FindByEmailAsync(request.Email);
-            
+            var user = await _signInManager.UserManager.FindByEmailAsync(request.Email);
+            if (user is null)
+                return NoContent();
+
+            var isThePasswordValid = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (!isThePasswordValid.Succeeded)
+                return Forbid();
+
+            var token = await GenerateToken(user);
+
+            return Ok(token);
+        }
+
+        [HttpPost("SignUp")]
+        public async Task<IActionResult> CreateAccount([FromBody] CreateAccountDto request,
+            CancellationToken cancellationToken = default)
+        { 
+            var user = new Customer
+            {
+                Email = request.Email,
+                UserName = request.Email.ToLowerInvariant().Trim(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                TwoFactorEnabled = false,
+            };
+
+            var result = await _signInManager.UserManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+                return BadRequest("Error while creating users");
+
+            // Add logic to send email confirmation in the future
+
+            var accessToken = await GenerateToken(user);
+
+            return StatusCode(201, accessToken);
+
+        }
+
+            private async Task<TokenResponseDto> GenerateToken(Customer user)
+        {
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var claims = new List<Claim> 
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,user.Id),
+                new Claim(JwtRegisteredClaimNames.UniqueName,user.UserName),
+            };
+
+            var key = Encoding.UTF8.GetBytes(_jwtSettings.Value.Key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Audience = _jwtSettings.Value.Audience,
+                Issuer = _jwtSettings.Value.Issuer,
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+            var accessToken = jwtSecurityTokenHandler.CreateToken(tokenDescriptor);
+
+            var token = jwtSecurityTokenHandler.WriteToken(accessToken);
+            var refreshToken = CryptoHelpers.GenerateRandomString(20);
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+
+            await _signInManager.UserManager.UpdateAsync(user);
+
+            return new TokenResponseDto
+            {
+                AccessToken = token ?? string.Empty,
+                RefreshToken = refreshToken
+            };
         }
     }
+    
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,7 +1,13 @@
 
 using Common.Core.Domain;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.Text;
+using WebApi.Configuration.Settings;
 
 namespace WebApi
 {
@@ -21,8 +27,59 @@ namespace WebApi
                 });
             });
 
+            builder.Services.AddCors(setupAction => setupAction.AddPolicy("FactoryPolicy", options =>
+            {
+                if (builder.Environment.IsDevelopment()) {
+                    options.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                } 
+                else {
+                    //If its not development, we need to specify the origins that are allowed to access the API from the appsetting.json
+                }
+                
+            }));
+
             builder.Services.AddControllers();
-            
+
+            //Identity
+            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+
+            builder.Services.AddIdentity<Customer, IdentityRole>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireUppercase = true;
+                options.Lockout = new LockoutOptions() { AllowedForNewUsers = false };
+
+            }).AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication(config =>
+            {
+                config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                config.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(config =>
+            {
+                config.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"] ?? string.Empty)),
+                    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                    ValidateActor = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.FromMinutes(5),
+                    // ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            builder.Services.AddAuthorization();
+     
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
